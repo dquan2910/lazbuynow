6284ec8323f9e14c56f06a87f99d8eb2394e7e40
dquan2910 <dongquanmaingoc2910@gmail.com>
2025-08-30 05:23:46 +0700

    fix bug, update proxy

--- function.js
+++ function.js
@@ -5,12 +5,12 @@ function Change()
     {
         document.getElementById("thongbaoLink").innerHTML = "Vui lòng điền link.";
     }
-    if(productLink.includes("lazada.vn/products/"))
+    if(productLink.includes("lazada.vn/products/") || productLink.includes("lazada.vn/i"))
     {
         var posOfTemp = productLink.indexOf(".html");
         if(posOfTemp == -1)
             document.getElementById("thongbaoLink").innerHTML = "Link chưa hợp lý";
-        ToBuyNowLink(productLink);    
+        ToBuyNowLink(productLink);
     }
     else if(productLink.includes("s.lazada.vn") || productLink.includes("c.lazada.vn"))
     {
@@ -24,45 +24,50 @@ function Change()
 }
 function ToBuyNowLink(productLink)
 {
-    var posOfTemp = productLink.indexOf(".html");
     document.getElementById("thongbaoLink").innerHTML = "";
+    var posOfTemp = productLink.indexOf(".html");
     productLink = productLink.substring(0, posOfTemp);
     var posOfSKUId = productLink.lastIndexOf("-s") + 2;
-    var SKUId = productLink.substring(posOfSKUId);
-    var qtyraw = document.getElementById("quantity").value;
-    let qty = 1;
-    if(qtyraw != "")
-        qty = parseInt(qtyraw, 10);
-    if(qty < 1)
-        qty = 1;
-    var itemId = productLink.substring(productLink.lastIndexOf("i") + 1, posOfSKUId - 2);
-    var finalLink = "https://www.lazada.vn/wow/gcp/vn/trade/shipping?spm=a2o4n.pdp_revamp.main_page.bottom_bar_main_button&buyParams=%7B%22items%22%3A%5B%7B%22itemId%22%3A%22" + itemId + "%22%2C%22skuId%22%3A%22" + SKUId + "%22%2C%22quantity%22%3A"+ qty + "%2C%22attributes%22%3Anull%7D%5D%7D&from_pdp_buy_now=1&pwa_true_login=1";
-    document.getElementById("finalLink").value = finalLink;
-    document.getElementById("hrefLink").setAttribute("href", finalLink);
+    if(posOfSKUId == 1) document.getElementById("thongbaoLink").innerHTML = "Link thiếu skuID. Thử lại link khác";
+    else
+    {
+        var SKUId = productLink.substring(posOfSKUId);
+        var qtyraw = document.getElementById("quantity").value;
+        let qty = 1;
+        if(qtyraw != "")
+            qty = parseInt(qtyraw, 10);
+        if(qty < 1)
+            qty = 1;
+        var itemId = productLink.substring(productLink.lastIndexOf("i") + 1, posOfSKUId - 2);
+        var finalLink = "https://www.lazada.vn/wow/gcp/vn/trade/shipping?spm=a2o4n.pdp_revamp.main_page.bottom_bar_main_button&buyParams=%7B%22items%22%3A%5B%7B%22itemId%22%3A%22" + itemId + "%22%2C%22skuId%22%3A%22" + SKUId + "%22%2C%22quantity%22%3A"+ qty + "%2C%22attributes%22%3Anull%7D%5D%7D&from_pdp_buy_now=1&pwa_true_login=1";
+        document.getElementById("finalLink").value = finalLink;
+        document.getElementById("hrefLink").setAttribute("href", finalLink);
+        document.getElementById("thongbaoLink").innerHTML = `Thành công. itemID: ${itemId} - skuID:${SKUId} - SL:${qty}`;
+    }
 }
 
 function FetchLink(productLink)
 {
-    const proxy = "https://api.allorigins.win/get?url=" + encodeURIComponent(productLink);
+    const proxy = "https://cosr-proxy.dongquanmaingoc2910.workers.dev/?url=" + encodeURIComponent(productLink);
+    document.getElementById("thongbaoLink").innerHTML = "Đang chuyển đổi....";
     fetch(proxy)
-    .then(res => res.json())
+    .then(r => r.json())
     .then(data => {
-    const html = data.contents;
-    const matches = html.match(/https:\/\/www\.lazada\.vn\/products\/[^\"]+/);
-    if (matches) {
-        var matchedUrl = matches?.[0]
-        ToBuyNowLink(matchedUrl);
-    } else {
-        document.getElementById("thongbaoLink").innerHTML = "Vui lòng dùng tạm link dài";
-    }
+        var newLink = data.longUrl;
+        if(newLink == productLink)
+            document.getElementById("thongbaoLink").innerHTML = "Link sai. Vui lòng kiểm tra lại"
+        else ToBuyNowLink(newLink);
   }).catch(err => document.getElementById("thongbaoLink").innerHTML = "Vui lòng dùng tạm link dài");
 
 }
+
 function ChangeQty(productLink)
 {
     if(productLink.includes("itemId%22%3A%") && productLink.includes("skuId%22%3A%") && productLink.includes("quantity%22%3A"))
     {
-        var linktoQty = productLink.substring(0, productLink.indexOf("quantity%22%3A") + 14);
+        var oldQtyPos =  productLink.indexOf("quantity%22%3A") + 14;
+        var oldQty = productLink.substring(oldQtyPos, productLink.indexOf("%2C%22attributes"));
+        var linktoQty = productLink.substring(0, oldQtyPos);
         var qtyraw = document.getElementById("quantity").value;
         let qty = 1;
         if(qtyraw != "")
@@ -72,6 +77,7 @@ function ChangeQty(productLink)
         var finalLink = linktoQty + qty + "%2C%22attributes%22%3Anull%7D%5D%7D&from_pdp_buy_now=1&pwa_true_login=1";
         document.getElementById("finalLink").value = finalLink;
         document.getElementById("hrefLink").setAttribute("href", finalLink);
+        document.getElementById("thongbaoLink").innerHTML = `Thành công. SL: ${oldQty} -> ${qty}`;
     }
     else
         document.getElementById("thongbaoLink").innerHTML = "Link buy now sai, thử lại link khác";
@@ -95,8 +101,6 @@ function Reset()
 {
     document.getElementById("productLink").value = "";
     document.getElementById("finalLink").value = "";
-    document.getElementById("APILink").innerHTML = "";
     document.getElementById("hrefLink").href = "#";
     document.getElementById("thongbaoLink").innerHTML = "";
-    document.getElementById("thongbaoAPI").innerHTML = "";
 }
